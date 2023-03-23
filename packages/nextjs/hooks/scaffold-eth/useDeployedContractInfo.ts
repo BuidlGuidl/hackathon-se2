e0eea46c2c05797436e20908bc874fd356c9f26e
Carlos Sánchez <oceanrdn@gmail.com>
2023-03-23 18:22:13 +0100

    Init

---
+++ packages/nextjs/hooks/scaffold-eth/useDeployedContractInfo.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Contract, ContractCodeStatus, ContractName } from "./contract.types";
+import { useIsMounted } from "usehooks-ts";
+import { useProvider } from "wagmi";
+import contracts from "~~/generated/hardhat_contracts";
+import scaffoldConfig from "~~/scaffold.config";
+
+/**
+ * Gets a deployed contract from `yarn deploy` generated files.
+ * @param contractName - name of deployed contract
+ */
+export const useDeployedContractInfo = <TContractName extends ContractName>(contractName: TContractName) => {
+  const isMounted = useIsMounted();
+  const deployedContract = contracts[scaffoldConfig.targetNetwork.id]?.[0]?.contracts?.[
+    contractName as ContractName
+  ] as Contract<TContractName>;
+  const [status, setStatus] = useState<ContractCodeStatus>(ContractCodeStatus.LOADING);
+  const provider = useProvider({ chainId: scaffoldConfig.targetNetwork.id });
+
+  useEffect(() => {
+    const checkContractDeployment = async () => {
+      if (!deployedContract) {
+        setStatus(ContractCodeStatus.NOT_FOUND);
+        return;
+      }
+      const code = await provider.getCode((deployedContract as Contract<TContractName>).address);
+
+      if (!isMounted()) {
+        return;
+      }
+      // If contract code is `0x` => no contract deployed on that address
+      if (code === "0x") {
+        setStatus(ContractCodeStatus.NOT_FOUND);
+        return;
+      }
+      setStatus(ContractCodeStatus.DEPLOYED);
+    };
+
+    checkContractDeployment();
+  }, [isMounted, contractName, deployedContract, provider]);
+
+  return {
+    data: status === ContractCodeStatus.DEPLOYED ? deployedContract : undefined,
+    isLoading: status === ContractCodeStatus.LOADING,
+  };
+};
---
+++ packages/nextjs/services/web3/wagmi-burner/burnerWalletConfig.ts
@@ -0,0 +1,34 @@
+import { Chain, Wallet } from "@rainbow-me/rainbowkit";
+import { hardhat } from "wagmi/chains";
+import {
+  BurnerConnector,
+  burnerWalletId,
+  burnerWalletName,
+  defaultBurnerChainId,
+} from "~~/services/web3/wagmi-burner/BurnerConnector";
+import { getTargetNetwork } from "~~/utils/scaffold-eth";
+
+export interface BurnerWalletOptions {
+  chains: Chain[];
+}
+
+/**
+ * Wagmi config for burner wallet
+ * @param param0
+ * @returns
+ */
+export const burnerWalletConfig = ({ chains }: BurnerWalletOptions): Wallet => ({
+  id: burnerWalletId,
+  name: burnerWalletName,
+  iconUrl: "https://avatars.githubusercontent.com/u/56928858?s=200&v=4",
+  iconBackground: "#0c2f78",
+  //todo add conditions to hide burner wallet
+  hidden: () => getTargetNetwork().id !== hardhat.id,
+  createConnector: () => {
+    const connector = new BurnerConnector({ chains, options: { defaultChainId: defaultBurnerChainId } });
+
+    return {
+      connector,
+    };
+  },
+});
